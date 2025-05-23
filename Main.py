0ced130f4d9e5f01564e2649677dcfbba40548dd
Troy Loeffler <troydloeffler@gmail.com>
2025-05-23 11:10:33 -0700

    Reenabled logging for neutral status

--- Main.py
+++ Main.py
@@ -37,9 +37,9 @@ def main():
             logging.info(f"Current time: {curtime}")
             
             if lan_ok and internet_ok:
-                #logging.info("Both LAN and Internet are reachable.")
+                logging.info("Both LAN and Internet are reachable.")
                 #print("✅ Both LAN and Internet are reachable.")
-                pass
+                #pass
                 
             elif lan_ok and not internet_ok:
                 #print("LAN is up, but Internet is down.")
